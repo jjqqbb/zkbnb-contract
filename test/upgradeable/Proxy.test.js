6252778cb2af6902a4cb9f853fdaab86d0464691
rain <rain.z@binance.com>
2023-04-28 10:15:19 +0300

    test: Add test case for proxy event

--- test/upgradeable/Proxy.test.js
+++ test/upgradeable/Proxy.test.js
@@ -86,10 +86,26 @@ describe('Proxy', function () {
       const mockGovernanceNew = await MockGovernance.deploy();
       await mockGovernanceNew.deployed();
 
-      await proxyGovernance.upgradeTarget(mockGovernanceNew.address, addr1.address);
-
       expect(mockGovernanceNew.upgrade).to.be.delegatedFrom(proxyGovernance.address);
       expect(mockGovernanceNew.upgrade).to.have.been.calledWith(addr1.address.toLowerCase());
+    });
+
+    it('upgradeTarget event', async function () {
+      const MockGovernance = await smock.mock('Governance', {
+        libraries: {
+          Utils: utils.address,
+        },
+      });
+      const mockGovernanceNew = await MockGovernance.deploy();
+      await mockGovernanceNew.deployed();
+
+      const upgrade = await proxyGovernance.upgradeTarget(mockGovernanceNew.address, addr1.address);
+      const receipt = await upgrade.wait();
+      const event = receipt.events.filter(({ event }) => {
+        return event === 'Upgraded';
+      });
+
+      expect(event[0]).to.be.not.null;
     });
 
     it('upgrade new `ZkBNBVerifier` target', async function () {
